69d7ff295450a93feefd9984dc9c197151cd9c1c
gooseberry <eric.desgroseilliers@gmail.com>
2022-02-06 11:45:29 -0500

    formating

--- decompress.sh
+++ decompress.sh
@@ -7,8 +7,11 @@ export TMPDIR=`mktemp -d /tmp/selfextract.XXXXX`
 
 ARCHIVE=`awk '/^__ARCHIVE_BELOW__/ {print NR + 1; exit 0; }' $0`
 
+# decompress the appended archive into the temp directory
 tail -n+$ARCHIVE $0 | tar xzv -C $TMPDIR
 
+# run the installation script that will replace all the
+# necessary files into their right place.
 CDIR=`pwd`
 cd $TMPDIR
 ./deploy-crostini.sh
