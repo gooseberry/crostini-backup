8ca238a085760bfd213cc0fec316a24db713ef08
gooseberry <gooseberry@users.noreply.github.com>
2022-03-21 18:20:21 -0400

    Update deploy-crostini.sh
    Added lines to add Lutris on the build

--- payload/deploy-crostini.sh
+++ payload/deploy-crostini.sh
@@ -17,7 +17,8 @@ REQUIRED_PACKAGES=(lsb-release \
   vim \
   mesa-utils \
   apt-file \
-  wine32)
+  wine32 \
+  lutris)
 
 extract_files () {
   echo
@@ -34,6 +35,12 @@ update_system () {
   echo "System updates completed."
   echo
 }
+
+add_lutris_repo () {
+  echo
+  echo "Adding Lutris Repos..."
+  echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_11/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
+  wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_11/Release.key -O- | sudo apt-key add -
 
 install_packages () {
   packages=$@
